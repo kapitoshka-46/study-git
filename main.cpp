0fc9dfa7beb0be550437cb9e7b73ad9e8b956640
Octocat& <3537256lol@gmail.com>
2025-07-25 00:29:30 +0700

    add func1 and func2

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+void func1() {
+    std::cout << "func1()" << std::endl;
+}
+
+void func2() {
+    std::cout << "func2()" << std::endl;
+}
+
+int main() {
+    func1();
+    func2();
+
+}
